458c8c4e0b9b60799f98dde653cc8a49bbcdbc79
ErangaFernando404 <erangafdo1994@gmail.com>
2026-04-07 12:06:06 +0530

    create apigetway

---
+++ api-gateway/src/main/java/lk/ijse/gdse/apigateway/fillter/AuthenticationFilter.java
@@ -0,0 +1,56 @@
+package lk.ijse.gdse.apigateway.fillter;
+
+import lk.ijse.gdse.apigateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "Missing or Invalid Authorization Header"
+                );
+            }
+
+
+            String token = authHeader.substring(7);
+
+            try {
+                jwtUtil.validateToken(token);
+
+                return chain.filter(exchange);
+
+            } catch (Exception e) {
+                throw new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "Invalid Token"
+                );
+            }
+        };
+    }
+}
--- api-gateway/src/main/java/lk/ijse/gdse/apigateway/util/JwtUtil.java
+++ api-gateway/src/main/java/lk/ijse/gdse/apigateway/util/JwtUtil.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse.apigateway.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+    public void validateToken(final String token) {
+        try {
+            String[] chunks = token.split("\\.");
+            if (chunks.length < 2) {
+                throw new RuntimeException("Invalid Token Format");
+            }
+        } catch (Exception e) {
+            System.out.println(" Token Error: " + e.getMessage());
+            throw new RuntimeException("Unauthorized access");
+        }
+    }
 }
